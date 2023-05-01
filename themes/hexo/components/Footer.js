c58f0a8b3566af843b17ba15cf77a5f99a11c4ba
lazyvip <alex13workspace@gmail.com>
2023-05-01 13:25:32 +0800

    1

--- themes/hexo/components/Footer.js
+++ themes/hexo/components/Footer.js
@@ -24,12 +24,11 @@ const Footer = ({ title }) => {
 
       <span className='hidden busuanzi_container_site_pv'>
             <i className='fas fa-eye'/><span className='px-1 busuanzi_value_site_pv'> </span>  </span>
+      
       <span className='pl-2 hidden busuanzi_container_site_uv'>
         <i className='fas fa-users'/> <span className='px-1 busuanzi_value_site_uv'> </span> </span>
         <br/>
         <h1>{title}</h1>
-        {/* <span className='text-xs '>Powered by <a href='https://github.com/tangly1024/NotionNext' className='underline dark:text-gray-300'>NotionNext {BLOG.VERSION}</a>.</span></span><br/> */}
-
     </footer>
   )
 }
