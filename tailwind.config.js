bef3d25c6a5328eb4d772e3342a1ea4ea25ac630
lazyvip <alex13workspace@gmail.com>
2024-08-12 15:01:02 +0800

    1

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,18 @@ module.exports = {
   darkMode: BLOG.APPEARANCE === 'class' ? 'media' : 'class', // or 'media' or 'class'
   theme: {
     fontFamily: fontFamilies,
+    fontSize: {
+      xs: '.75rem',
+      sm: '.875rem',
+      base: '1.5rem',
+      lg: '1.125rem',
+      xl: '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem'
+    },
     screens: {
       sm: '540px',
       // => @media (min-width: 576px) { ... }
